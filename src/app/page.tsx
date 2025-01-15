d89267aebff32418d20bfc9660cf706def808bbc
jihun <jihunv8@gmail.com>
2025-01-15 18:21:42 +0900

    refactor: "/"에 진입시 "/chord-dictionary"로 리다이렉트하도록 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Header from '@/app/_components/Header';
+import { redirect } from 'next/navigation';
 
 export default function Home() {
+  redirect('/chord-dictionary');
   return (
     <div>
       <Header />
